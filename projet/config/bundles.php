67d2ed0301a4a3c16b654b4527f05f88afb1aeb9
Mourad Ben Fradj <mourad.ben.fradj@gmail.com>
2024-10-28 12:06:25 +0000

    loger

--- projet/config/bundles.php
+++ projet/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
