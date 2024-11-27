3c20ae335268bca5a42f0276e483db579a2e623c
Mourad Ben Fradj <mourad.ben.fradj@gmail.com>
2024-11-28 00:18:36 +0100

    correction babel

--- projet/config/bundles.php
+++ projet/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
